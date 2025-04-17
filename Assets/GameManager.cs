2f300d50ba1949c5b9c6ab68e08cb5cf876ff4da
Marcus Sjöblom <marsj402@student.liu.se>
2025-04-17 16:38:55 +0200

    chore: Added win condition
    Now the game ends if a player achivies 10 points, restart button restarts the game

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine;
 using TMPro;
@@ -15,12 +16,19 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI leftScoreText;
     public TextMeshProUGUI rightScoreText;
 
+    [Header("End Screen UI")]
+    public GameObject endScreenPanel;
+    public TextMeshProUGUI winText;
+
     [HideInInspector] public string scorer = "";
     [HideInInspector] public int leftScore;
     [HideInInspector] public int rightScore;
 
+    private const int MaxScore = 10;
+
     private void Awake()
     {
+        endScreenPanel.SetActive(false);
         if (Instance == null) { Instance = this; }
         else { Destroy(gameObject); }
     }
@@ -35,7 +43,7 @@ public class GameManager : MonoBehaviour
         scorer = "left";
         leftScore++;
         leftScoreText.text = leftScore.ToString();
-        SpawnBallWithDelay();
+        CheckForWin();
     }
 
     public void AddPointToRight()
@@ -43,7 +51,30 @@ public class GameManager : MonoBehaviour
         scorer = "right";
         rightScore++;
         rightScoreText.text = rightScore.ToString();
-        SpawnBallWithDelay();
+        CheckForWin();
+    }
+
+    private void CheckForWin()
+    {
+        if (leftScore >= MaxScore)
+        {
+            ShowEndScreen("Player 1 Wins!");
+        }
+        else if (rightScore >= MaxScore)
+        {
+            ShowEndScreen("Player 2 Wins!");
+        }
+        else
+        {
+            SpawnBallWithDelay();
+        }
+    }
+
+    private void ShowEndScreen(string message)
+    {
+        Time.timeScale = 0f;
+        winText.text = message;
+        endScreenPanel.SetActive(true);
     }
 
     private void SpawnBallWithDelay()
@@ -55,5 +86,18 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(ballLaunchDelay);
         Instantiate(ballPrefab, ballSpawnPoint.position, Quaternion.identity);
+    }
+
+    // BUTTON CALLS
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
