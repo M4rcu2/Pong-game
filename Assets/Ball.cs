643ea8ce15cebdce7a2c246eabd23f366e6fe2e9
Marcus Sjöblom <marsj402@student.liu.se>
2025-04-18 15:13:04 +0200

    fix: Added small force when ball touches border
    Before the ball could "slide" along the border, Now a small force is applied so it leaves the border

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -29,14 +29,41 @@ public class Ball : MonoBehaviour
         rb.freezeRotation = true;
         rb.velocity = new Vector2(xVelocity * startingSpeed, yVelocity * startingSpeed);
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (bounceSounds.Length <= 0 || _audioSource == null) return;
+        if (bounceSounds.Length > 0 && _audioSource != null)
+        {
+            var randomClip = bounceSounds[Random.Range(0, bounceSounds.Length)];
+            _audioSource.PlayOneShot(randomClip);
+        }
         
-        var randomClip = bounceSounds[Random.Range(0, bounceSounds.Length)];
-        _audioSource.PlayOneShot(randomClip);
+        if (collision.gameObject.CompareTag("BorderUp"))
+        {
+            const float directionY = -1f;
+            var forceAmount = startingSpeed * 0.1f;
+            rb.AddForce(new Vector2(0f, directionY * forceAmount), ForceMode2D.Impulse);
+        } else if (collision.gameObject.CompareTag("BorderDown"))
+        {
+            const float directionY = 1f;
+            var forceAmount = startingSpeed * 0.1f;
+            rb.AddForce(new Vector2(0f, directionY * forceAmount), ForceMode2D.Impulse);
+        }
+        
+        if (!collision.gameObject.CompareTag("Paddle")) return;
+        
+        var paddleRb = collision.gameObject.GetComponent<Rigidbody2D>();
+        
+        if (paddleRb == null) return;
+        
+        var paddleYVelocity = paddleRb.velocity.y;
+        const float influenceFactor = 0.9f;
+        var newVelocity = rb.velocity;
+        newVelocity.y += paddleYVelocity * influenceFactor;
+        rb.velocity = newVelocity;
     }
+
+
 
     private void OnBecameInvisible()
     {
